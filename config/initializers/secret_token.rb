a552abc7d9709dc09cf4f86852f5fe8aa0273882
Oluwafemi A <favourfield@gmail.com>
2010-11-23 17:23:05 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LegacySocialBlog::Application.config.secret_token = '0d5d261ce7c5622d30611acacef2e5dc9837b950f3eafa1ff19bfa5f163c0ba7edec0a1082bac769604a5067d1e74e9d5b4fb3dacd068ec544479a49425828ed'
