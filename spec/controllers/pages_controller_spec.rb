3ee26b0634f385e3da2c624d4b304da08e1237b1
Oluwafemi A <favourfield@gmail.com>
2010-11-25 17:20:41 +0100

    Trashed default rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+  @base_title = "Legacy Social Blog | "
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,8 +16,8 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                          :content =>
-                               "Legacy Social Blog | Home")
+                          :content => @base_title +
+                                "Home")
     end
   end
 
@@ -26,8 +30,8 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                          :content => 
-                             "Legacy Social Blog | Contact")
+                          :content => @base_title + 
+                             "Contact")
     end
   end
 
@@ -40,8 +44,22 @@ describe PagesController do
     it "should have the right title" do
       get 'about' 
       response.should have_selector("title",
-                      :content =>
-                          "Legacy Social Blog | About")
+                      :content => @base_title +
+                          "About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help' 
+      response.should have_selector("title",
+                      :content => @base_title +
+                          "Help")
     end
   end
    
