1556fd2436b8ef731dcd9b118aaed4d62f3d109c
Oluwafemi A <favourfield@gmail.com>
2010-12-03 14:05:25 +0100

    Added user microposts complete

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
 
-  before_filter :authenticate
+  before_filter :authenticate    #, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
@@ -8,11 +9,20 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_path
     else
-      @feed_items =[]
+      @feed_items = []
       render 'pages/home'
     end
   end
 
   def destroy
+    @micropost.destroy
+    redirect_back_or root_path
+  end
+
+  private
+
+  def authorized_user
+    @micropost = Micropost.find(params[:id])
+    redirect_to root_path unless current_user?(@micropost.user)
   end
 end
