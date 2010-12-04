34fc0e9a3c7af015d2f1d4f2f32e30e489041bc5
Oluwafemi A <favourfield@gmail.com>
2010-12-04 13:09:33 +0100

    Added user-following......Miraculous

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -37,4 +37,34 @@ describe Micropost do
       @user.microposts.build(:content => 'p' * 141).should_not be_valid
     end
   end
+
+  describe "from_users_followed_by" do
+
+    before(:each) do
+      @other_user = Factory(:user, :email => Factory.next(:email))
+      @third_user = Factory(:user, :email => Factory.next(:email))
+
+      @user_post = @user.microposts.create!(:content => "They're the ones who will be called on the carpet")
+      @other_post = @other_user.microposts.create!(:content => "Friends, when life gets really")
+      @third_post = @third_user.microposts.create!(:content => "It's judgment time for God's own family")
+
+      @user.follow!(@other_user)
+    end
+
+    it "should have a from_users_followed_by class method" do
+      Micropost.should respond_to(:from_users_followed_by)
+    end
+
+    it "should include the followed user's microposts" do
+      Micropost.from_users_followed_by(@user).should include(@other_post)
+    end
+
+    it "should include the user's own microposts" do
+      Micropost.from_users_followed_by(@user).should include(@user_post)
+    end
+
+    it "should not include an unfollowed user's micropost" do
+      Micropost.from_users_followed_by(@user).should_not include(@third_post)
+    end
+  end    
 end
