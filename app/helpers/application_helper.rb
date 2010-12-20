4034bfd07ddf990dcd75acf93fe7f1db9a0d1c64
Oluwafemi A <favourfield@gmail.com>
2010-12-20 22:19:58 +0100

    revamping the ui...2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
 #Return a title on a per-page basis.
 def title
-base_title = "Legacy Social Blog"
-if @title.nil? 
-base_title
-else
-"#{base_title} | #{@title}"
-end
+    base_title = "Legacy Social Blog"
+  if @title.nil? 
+    base_title
+  else
+    "#{base_title} | #{@title}"
+  end
 end
 
 def logo
-image_tag("logo.png", :alt => "Legacy Blog", :class =>"round")
+  image_tag("logo.png", :alt => "Legacy Blog", :class =>"round")
 end
 
 end
